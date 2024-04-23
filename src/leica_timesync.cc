c8da8e79af27d8d3fc93037bb18d324da1cfa58c
TimonMathis <timathis@ethz.ch>
2024-04-23 16:32:43 +0200

    frame removed and back to only ros time now

--- src/leica_timesync.cc
+++ src/leica_timesync.cc
@@ -6,20 +6,22 @@ LeicaTimesync::LeicaTimesync() : nh_("~")
     point_pub_ = nh_.advertise<geometry_msgs::PointStamped>("/leica/position/rostime", 10);
 
     imu_sub_ = nh_.subscribe<sensor_msgs::Imu>("/imu/data_raw", 10, &LeicaTimesync::imuCallback, this, ros::TransportHints().tcpNoDelay());
-    imu_pub_ = nh_.advertise<sensor_msgs::Imu>("/leica/imu/rostime", 10);
+    imu_pub_ = nh_.advertise<sensor_msgs::Imu>("/imu/data_raw/rostime", 10);
 }
 
 void LeicaTimesync::pointCallback(const geometry_msgs::PointStamped::ConstPtr& msg)
 {
     geometry_msgs::PointStamped output_msg = *msg;
-    output_msg.header.stamp = ros::Time::now() - ros::Duration(0.06); // Subtract a delay of 60ms from the current timestamp
+    output_msg.header.stamp = ros::Time::now(); // Subtract a delay of 60ms from the current timestamp"
+    output_msg.header.frame_id = "";
     point_pub_.publish(output_msg);
 }
 
 void LeicaTimesync::imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
     sensor_msgs::Imu output_msg = *msg;
-    output_msg.header.stamp = ros::Time::now() - ros::Duration(0.003); // Subtract a delay of 3ms from the current timestamp
+    output_msg.header.stamp = ros::Time::now(); // Subtract a delay of 3ms from the current timestamp
+    output_msg.header.frame_id = "";
     imu_pub_.publish(output_msg);
 }
 
